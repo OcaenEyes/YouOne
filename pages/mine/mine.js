6d4ed466708808ed209d2806f9ec9fbee36017b7
OcaenEyes <1450136519@qq.com>
2018-10-27 22:43:20 +0800

    0.0.1
    0.0.1

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -4,62 +4,102 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+
+    "res": 0,
+    "data": [
+      {
+        "content_type": 0,
+        "category": 0,
+        "id": "2240",
+        "date": "2018/10/25",
+        "title": "VOL.2210",
+        "url": "http://m.wufazhuce.com/one/2240",
+        "img_url": "http://image.wufazhuce.com/FqSsjXB-DVascO_O_nfQjEWOv_gg",
+        "picture_author": "摄影 | Ahmet Ali Agir",
+        "content": "恋爱说到底，就是游刃有余的人才能干的事。",
+        "text_authors": "《无法成为野兽的我们》"
+      },
+      {
+        "content_type": 0,
+        "category": 0,
+        "id": "2240",
+        "date": "2018/10/25",
+        "title": "VOL.2210",
+        "url": "http://m.wufazhuce.com/one/2240",
+        "img_url": "http://image.wufazhuce.com/FqSsjXB-DVascO_O_nfQjEWOv_gg",
+        "picture_author": "摄影 | YSilhouette",
+        "content": "恋爱说到底，就是游刃有余的人才能干的事。",
+        "text_authors": "《无法成为野兽的我们》"
+      },
+      {
+        "content_type": 0,
+        "category": 0,
+        "id": "2240",
+        "date": "2018/10/25",
+        "title": "VOL.2210",
+        "url": "http://m.wufazhuce.com/one/2240",
+        "img_url": "http://image.wufazhuce.com/FqSsjXB-DVascO_O_nfQjEWOv_gg",
+        "picture_author": "摄影 | YSilhouette",
+        "content": "恋爱说到底，就是游刃有余的人才能干的事。",
+        "text_authors": "《无法成为野兽的我们》"
+      }
+    ]
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    
+  onLoad: function(options) {
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-    
+  onReady: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-    
+  onShow: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-    
+  onHide: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-    
+  onUnload: function() {
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-    
+  onPullDownRefresh: function() {
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-    
+  onReachBottom: function() {
+
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-    
+  onShareAppMessage: function() {
+
   }
 })
